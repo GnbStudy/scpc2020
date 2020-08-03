eaee9ee3aa503a54a8b2d61413777d27f416528a
Dohun Kim <yh08037@gmail.com>
2020-08-03 12:58:17 +0900

    연습문제 8번 풀이

---
+++ 연습문제/코드_김도훈/8_블럭없애기.cpp
@@ -0,0 +1,81 @@
+/*
+You should use the statndard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful. 
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int Answer;
+int h[100002];
+int c[100002];
+
+int main(int argc, char **argv)
+{
+    int T, test_case;
+    int N, i, m;
+    bool isAllZero;
+    /*
+	   The freopen function below opens input.txt file in read only mode, and afterward,
+	   the program will read from input.txt file instead of standard(keyboard) input.
+	   To test your program, you may save input data in input.txt file,
+	   and use freopen function to read from the file when using cin function.
+	   You may remove the comment symbols(//) in the below statement and use it.
+	   Use #include<cstdio> or #include <stdio.h> to use the function in your program.
+	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+	 */
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> T;
+    for (test_case = 0; test_case < T; test_case++)
+    {
+
+        Answer = 0;
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        cin >> N;
+        isAllZero = false;
+
+        for (i = 1; i <= N; i++)
+            cin >> h[i];
+
+        while (!isAllZero)
+        {
+
+            for (i = 1; i <= N; i++)
+            {
+                m = min(h[i - 1], h[i + 1]);
+                c[i] = min(m, h[i] - 1);
+            }
+
+            Answer++;
+
+            isAllZero = true;
+            for (i = 1; i <= N; i++)
+            {
+                if (c[i] > 0)
+                {
+                    isAllZero = false;
+                    break;
+                }
+            }
+
+            for (i = 1; i <= N; i++)
+                h[i] = c[i];
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Print the answer to standard output(screen).
+        cout << "Case #" << test_case + 1 << endl;
+        cout << Answer << endl;
+    }
+
+    return 0; //Your program should return 0 on normal termination.
+}
